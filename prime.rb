97565d8e1bb38d386ba3820fb85d79538d5f45f2
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-19 15:26:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,22 @@
-require 'prime'
+def isprime(n):
+    """Returns True if n is prime."""
+    if n == 2:
+        return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
 
-def is_prime?(num)
-  return Prime.prime?(num)  #=> true
-end
+    i = 5
+    w = 2
+
+    while i * i <= n:
+        if n % i == 0:
+            return False
+
+        i += w
+        w = 6 - w
+
+    return True
